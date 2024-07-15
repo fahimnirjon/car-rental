df8c17212ce0a9bb6e53c66ed6ed081118b75a9f
fahimnirjon <fahimnirjon21@gmail.com>
2024-07-16 00:15:27 +0600

    adedd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BiSolidSun, BiSolidMoon } from "react-icons/bi";
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
 import ResponsiveMenu from "./ResponsiveMenu";
